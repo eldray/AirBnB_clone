23f489c14b773924a1d2b89e84cd52728f9f7a50
eldray <emk.appiah@gmail.com>
2023-02-13 00:31:37 +0000

    update console

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,7 @@ class BaseModel:
         """
         String rep of class BaseModel
         """
-        return f"[{self.__class__.__name__}] ({self.id}) {self.__dict__}"
+        return f("[{self.__class__.__name__}] ({self.id}) {self.__dict__}")
 
     def save(self):
         """
